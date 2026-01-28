e531f511d1d0fd385cc738920a18dd3f66271cc6
Frosty-J <60154347+Frosty-J@users.noreply.github.com>
2026-01-28 05:13:51 +0000

    v2, get code for authentication

--- content.js
+++ content.js
@@ -1,3 +1,17 @@
-Object.defineProperty(window, '_elyPreValidated', {
-  value: true,
-});
+let code = "";
+const field = document.getElementById("premium-code");
+
+window.alert = function() {};
+
+const fcc = String.fromCharCode;
+String.fromCharCode = function(...args) {
+  const result = fcc.apply(this, args);
+  if (args.length === 1) code += result;
+  return result;
+};
+
+if (field && typeof window.validateCode === 'function') {
+  window.validateCode();
+  field.value = code;
+  window.validateCode();
+}
